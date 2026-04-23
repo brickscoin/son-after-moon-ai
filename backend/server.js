222fe627e44bf208c55cb16cda06f8f2c44033f7
brickscoin <ranjanrounak62@gmail.com>
2026-04-23 06:39:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // =============================================
 // MIDDLEWARE
 // =============================================
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use(express.static('public'));
 
